a7b7e3f8f1b317b26542ec3a225d7d3359583c60
harshkg2001 <harshvsharsh2001@gmail.com>
2024-07-11 23:09:53 +0530

    new intro solutions

--- 1_Introductory Problems/14_tower_of_hanoi.cpp
+++ 1_Introductory Problems/14_tower_of_hanoi.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 #define int long long
 
+void hanoi(int n, int from, int to, int aux, vector<pair<int, int>> &v)
+{
+    if(n==1)
+    {
+        v.push_back({from, to});
+        return;
+    }
+
+    hanoi(n-1, from, aux, to, v);
+    v.push_back({from, to});
+    hanoi(n-1, aux, to, from, v);
+}
+
 int32_t main()
 {
-    
+    int n;
+    cin>>n;
+
+    vector<pair<int, int>> v;
+    hanoi(n, 1, 3, 2, v);
+
+    cout<<v.size()<<"\n";
+    for(int i=0; i<v.size(); i++)
+        cout<<v[i].first<<" "<<v[i].second<<"\n";
+
     return 0;
 }
--- 1_Introductory Problems/16_apple_division.cpp
+++ 1_Introductory Problems/16_apple_division.cpp
@@ -1,9 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+#define mod 1000000007
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+int mod_exp(int a, int b)
+{
+    int res=1;
+    while(b)
+    {
+        if(b%2)
+            res=(res*a)%mod;
+
+        a=(a*a)%mod;
+        b/=2;
+    }
+    return res;
+}
 
 int32_t main()
 {
+    fastio;
+
     int n;
     cin>>n;
 
@@ -11,28 +29,25 @@ int32_t main()
     for(int i=0; i<n; i++)
         cin>>v[i];
 
-    int mn=LLONG_MAX;
-    int sum=accumulate(v.begin(), v.end(), 0);
+    int sum=accumulate(v.begin(), v.end(), (int)0);
+    int x = mod_exp(2, n);
 
-    int lim=1;
-    while(n--)
-        lim*=2;
-
-    for(int i=0; i<lim; i++)
+    int ans=LLONG_MAX;
+    for(int i=0; i<x/2; i++)
     {
-        int val=0;
-        int x=i, j=0;
-        while(x && j<v.size())
+        int j=0, val=i, temp=0;
+        while(val)
         {
-            if(x%2)
-                val+=v[j];
+            if(val%2)
+                temp+=v[j];
 
             j+=1;
-            x/=2;
+            val/=2;
         }
-        mn=min(mn, abs(sum-2*val));
-    }
 
-    cout<<mn<<"\n";
+        ans=min(ans, abs(sum-2*temp));
+    }
+    cout<<ans<<"\n";
+
     return 0;
 }
